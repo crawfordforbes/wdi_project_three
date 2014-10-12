a1970c450dd94ca3c5807585b55e901a13d133e3
brendee <brenda.dargan@gmail.com>
2014-10-12 15:28:29 -0400

    Added nav hover images

---
+++ freeday_app/public/javascripts/scripts.js
@@ -0,0 +1,22 @@
+$(function(){
+
+// Splash page navigation rollover
+   function createMouseRollover(createIcon) {
+      createIcon.src = "./create_icon_gray.png";
+    }
+    
+    function createMouseOut(createIconHover) {
+      createIconHover.src = "./create_icon.png";
+    }
+    
+   	function joinMouseRollover(joinIcon) {
+      joinIcon.src = "./join_icon_gray.png";
+    }
+    
+    function joinMouseOut(joinIconHover) {
+      joinIconHover.src = "./join_icon.png";
+    }
+
+// 
+
+});
