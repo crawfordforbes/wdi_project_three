2dde7074fc78c06f5eec0b78cfc675a7221e67af
Clayton Albachten <clay.reed.a@gmail.com>
2014-10-12 14:20:42 -0400

    draft routes logic

--- freeday_app/server.rb
+++ freeday_app/server.rb
@@ -17,70 +17,113 @@ end
 # events 
 
 get("/events") do 
+  Event.all.to_json 
 end 
 
-post("/events") do 
+post("/events") do
+  event_json = JSON.parse(request.body.read)  
+  Event.create(
+        name: event_json["name"],
+     zipcode: event_json["zipcode"],
+    deadline: Date.new(event_json["deadline"])
+    ).to_json
 end 
 
-# we haven't talked about deleting events,
-# maybe the delete event logic 
-# should include sending out the final 
-# report via SendGrid, etc. before 
-# actual deletion - CRA  
 delete("/events/:id") do 
+  # send out the emails via sendgrid, etc, then 
+  # destroy all associated objs 
+  Membership.destroy_all(event_id: params[:id])
+  Window.destroy_all(event_id: params[:id])
+  Activity.destroy_all(event_id: params[:id])
+  Event.destroy(params[:id]) 
 end 
 
 get("/events/:id") do 
+  Event.find_by_id(params[:id]).to_json
 end 
 
 put("/events/:id") do 
+  the_event = Event.find_by_id(params[:id])
+  the_event.update(:upvotes, 
+    the_event[:upvotes] + 1)
+  the_event.to_json 
 end 
 
 get("/events/:id/activities") do 
+  Activity.where(:event_id, params[:id]).to_json
 end 
 
-get("/events/:id/windows") do 
+get("/events/:id/windows") do
+  Window.where(:event_id, params[:id]).to_json 
 end 
 
 get("/events/:id/memberships") do 
+  Membership.where(:event_id, params[:id]).to_json
 end 
 
 # activities # 
 
-post("/activities") do 
+post("/activities") do
+  activity_json = JSON.parse(request.body.read) 
+  Activity.create(
+       event_id: activity_json["event_id"], 
+        upvotes: 1, 
+           name: activity_json["name"], 
+        address: activity_json["address"], 
+            url: activity_json["url"], 
+    description: activity_json["description"], 
+      window_id: activity_json["window_id"]
+
+    ).to_json  
 end 
 
-# not including a delete route for activities 
-# activities are deleted when an event is 
-
 put("/activities/:id") do 
+  the_activity = Activity.find_by_id(params[:id])
+  the_activity.update(:upvotes, 
+    the_activity.upvotes + 1).to_json
 end 
 
 # windows # 
 
-post("/windows") do 
+post("/windows") do
+  window_json = JSON.parse(request.body.read)  
+  Window.create(
+    event_id: window_json["event_id"],
+     upvotes: 0,
+         day: Date.new(window_json["day"])
+    ).to_json
 end 
 
-put("/windows") do 
-end 
-
-# for some reason it seems more reasonable 
-# to me one might   
-delete("/windows") do 
+put("/windows/:id") do
+  the_window = Window.find_by_id(params[:id])
+  the_window.update(:upvotes, 
+    the_window.upvotes + 1).to_json
 end 
 
 # memberships # 
 
-post("/memberships") do 
+post("/memberships") do
+  membership_json = JSON.parse(request.body.read)  
+  Membership.create(
+    people_id: membership_json["people_id"],
+     event_id: membership_json["event_id"]
+    ).to_json 
 end 
 
 # people #
 
 get("/people") do 
+  Person.all.to_json
 end 
 
-post("/people") do 
+post("/people") do
+  person_json = JSON.parse(request.body.read)  
+  Person.create(
+     name: person_json["name"], 
+    email: person_json["email"]
+    ).to_json
 end 
 
 get("/people/:id") do 
+  Person.find_by_id(params[:id]).to_json
 end 
