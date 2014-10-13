abb474071739fd20538bdd04c9847f8a5c90eea2
Clayton Albachten <clay.reed.a@gmail.com>
2014-10-13 16:51:17 -0400

    some highlighting working on calendar

--- freeday_app/public/javascripts/scripts.js
+++ freeday_app/public/javascripts/scripts.js
@@ -18,9 +18,40 @@ $(function(){
     }
 
     $("#dates").multiDatesPicker({
-      onClose: function(d){
-        console.log(d);
+      onClose: function(datesString){
+        sendWindowDates(datesString);
+        /* TO WHOMSOEVER WRITES THE AJAX CALLS: 
+        I think what we actually
+        want to do here is, onClose, 
+        set the value of this input#dates element
+        to the value of dateString.
+        When the submit button is pressed, 
+        we will require a function that implements the following:  
+        1) break the dateString into X different 
+        dateStrings (dateStrings are seperated by the substring ", "), 
+        2) send off X different AJAX requests 
+        3) to create X window objs on the server, which are then saved 
+        to the db, associated w/ the Event in Question. */ 
+        
 
       }
     });
  });
+
+function sendWindowDates(datesStr, eventId) {
+  var eventId = 1;
+  var dates = datesStr.split(", ");
+  $.each(dates, function(idx, dateStr) {
+    var theWindowJSON = {
+      event_id: eventId,
+      day: dateStr
+    };
+    $.post(
+      "/windows",
+      theWindowJSON,
+      function(response) {
+        console.log(response);
+      }); 
+  });
+}
+
